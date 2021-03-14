68722452c92674d55f8ed1f8af4c5bce0365c424
aprilahijriyan <hijriyan23@gmail.com>
2021-03-14 07:44:20 +0700

    🐛 FIX: Adding tables role_links to associate roles to the user

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, UnicodeText
+from sqlalchemy import Column, ForeignKey, Integer, UnicodeText
 from sqlalchemy.orm import relationship
 from zemfrog.globals import db
 from zemfrog.mixins import LogMixin, PermissionMixin, RoleMixin, UserMixin
@@ -8,15 +8,29 @@ class User(UserMixin, db.Model):
     nickname = Column(UnicodeText)
     profile_image = Column(UnicodeText)
     cover_image = Column(UnicodeText)
-    articles = relationship("Article")
+    roles = relationship("Role", secondary="role_links", lazy="dynamic")
 
 
 class Role(RoleMixin, db.Model):
-    pass
+    permissions = relationship(
+        "Permission", secondary="permission_links", lazy="dynamic"
+    )
+
+
+class RoleLinks(db.Model):
+    id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey("user.id"))
+    role_id = Column(ForeignKey("role.id"))
 
 
 class Permission(PermissionMixin, db.Model):
     pass
+
+
+class PermissionLinks(db.Model):
+    id = Column(Integer, primary_key=True)
+    role_id = Column(ForeignKey("role.id"))
+    permission_id = Column(ForeignKey("permission.id"))
 
 
 class Log(LogMixin, db.Model):
